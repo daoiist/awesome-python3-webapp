311dbef765e5019d39cdf847cc224be057756371
daoiist <whj.527@gmail.com>
2017-11-24 16:53:05 +0800

    11.24

--- www/app.py
+++ www/app.py
@@ -8,6 +8,8 @@ from jinja2 import Environment, FileSystemLoader
 
 from coroweb import add_routes, add_static
 
+import orm
+
 def init_jinja2(app, **kw):
     logging.info('init jinja2...')
     options = dict(
@@ -100,6 +102,7 @@ def datetime_filter(t):
     return u'%s年%s月%s日' % (dt.year, dt.month, dt.day)
 
 async def init(loop):
+    await orm.create_pool(loop=loop, host='127.0.0.1', port=3306, user='root', password='123456ss', db='aspw')
     app = web.Application(loop=loop, middlewares=[
         logger_factory, response_factory
     ])
--- www/handlers.py
+++ www/handlers.py
@@ -4,8 +4,12 @@ import re, time, json, logging, hashlib, base64, asyncio
 
 from coroweb import get, post
 
+from models import User, next_id
+
 @get('/')
-def index(request):
+async def index(request):
+    users = await User.findAll()
     return {
-        '__template__': 'test.html'
+        '__template__': 'test.html',
+        'users': users
     }
